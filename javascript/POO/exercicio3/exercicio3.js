8f0280867d845dc788e54238dcf2ee0948443e47
Leonardo-Pinhoo <leonardo.rodriguesguidolim@gmail.com>
2026-08-06 11:54:46 -0300

    exercices are done in the class

---
+++ javascript/POO/exercicio3/exercicio3.js
@@ -0,0 +1,38 @@
+class Produto{
+    constructor(nome, preco, estoque){
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+
+    vender(quantidade){
+        this.estoque -= quantidade;
+        return `Produto ${this.nome} vendido com sucesso`;
+    }
+
+    repor(quantidade){
+        this.estoque += quantidade;
+        return `Produto ${this.nome} reposto com sucesso`;
+    }
+
+    alterarPreco(novoPreco){
+        this.preco = novoPreco;
+        return `Produto ${this.nome} com novo preço ${novoPreco}`;
+    }
+    
+}
+
+const produto1 = new Produto("Produto 1", 10, 10);
+console.log(produto1.vender(5));
+console.log(produto1.repor(5));
+console.log(produto1.alterarPreco(15));
+
+const produto2 = new Produto("Produto 2", 20, 20);
+console.log(produto2.vender(10));
+console.log(produto2.repor(10));
+console.log(produto2.alterarPreco(25));
+
+const produto3 = new Produto("Produto 3", 30, 30);
+console.log(produto3.vender(15));
+console.log(produto3.repor(15));
+console.log(produto3.alterarPreco(35));
